54616a982d2c0b0d6b57aec2a8a84a5ef76cc88a
Caleb Gray <hey@calebgray.com>
2020-06-09 20:45:10 -0700

    rsagen!

--- docs/rsagen.js
+++ docs/rsagen.js
@@ -1,13 +1,11 @@
 'use strict';
 
+const go = new Go();
 const decoder = new TextDecoder('utf-8');
 global.fs.writeSync = function(fd, buf) {
   postMessage(decoder.decode(buf));
   return buf.length;
 };
-
-const keyStrength = !window.location.hash ? '2048' : window.location.hash.substring(1);
-const go = new Go();
 onmessage = function(keyStrength) {
   go.argv = ['rsagen.wasm', keyStrength];
   WebAssembly.instantiateStreaming(fetch('rsagen.wasm', { cache: 'force-cache' }), go.importObject).then((result) => {
